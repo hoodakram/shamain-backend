a095e724734289e0a13070131226a185aaea9dd5
hoodakram <140316526+hoodakram@users.noreply.github.com>
2026-03-06 13:46:49 +0500

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: ['https://shamain.store', 'https://www.shamain.store'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
