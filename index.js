fac3d64373a7df1017ed2ade6921e19b9dfadc4c
hoodakram <140316526+hoodakram@users.noreply.github.com>
2026-03-06 09:16:02 +0500

    again index

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: true, // allow all origins
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.use(cors());// handle preflight requests
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
@@ -23,10 +23,13 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log("MongoDB Error:", err.message || JSON.stringify(err)));
 
 app.use("/api/products", productRoutes);
-app.use("/uploads", express.static("uploads"));
-
 
 console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
